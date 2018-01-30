0529d5d1f1818d52cab2f32cf81f32ef5f15b09f
saturn <vmto@qq.com>
2018-01-30 22:04:11 +0800

    update

--- app.py
+++ app.py
@@ -7,18 +7,23 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
-def login():
+def index_login():
     return render_template('login.html')
 
 
 @app.route('/home', methods=['POST', 'GET'])
-def home():
+def page_home():
     return render_template('home.html')
 
 
 @app.route('/menus', methods=['POST', 'GET'])
-def menus():
+def page_menus():
     return render_template('page-menu.html')
+
+
+@app.route('/list', methods=['POST', 'GET'])
+def page_list():
+    return render_template('page-list.html')
 
 
 @app.route('/api_datagrid', methods=['POST', 'GET'])
@@ -32,7 +37,7 @@ def api_menus():
 
 
 @app.route('/form', methods=['POST', 'GET'])
-def form():
+def api_form():
     return jsonify({
         'code': 200,
         'msg': '数据保存成功！',
